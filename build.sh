8f329722e0a1f4f4608b97bb060bfef7bb3732ae
spayke3 <spaykedotimas892@gmail.com>
2024-08-23 05:18:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm -rf external/chromium-webview/prebuilt/*
+rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+
+
+
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 rm -rf lineage_build_unified
 git clone https://github.com/spayke3/lineage_build_unified lineage_build_unified -b patch-1

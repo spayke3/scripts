003201205ee196dcb97f329bb740ab06f94cdf87
spayke3 <spaykedotimas892@gmail.com>
2024-08-23 04:56:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
-
+rm -rf lineage_build_unified
 git clone https://github.com/spayke3/lineage_build_unified lineage_build_unified -b patch-1
 git clone https://github.com/AndyCGYan/lineage_patches_unified lineage_patches_unified -b lineage-21-td
 

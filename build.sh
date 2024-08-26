ae00796c19a5d98920a57a3ba3d1f6eeaaf202b6
spayke3 <spaykedotimas892@gmail.com>
2024-08-26 18:47:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuil
 repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 rm -rf lineage_build_unified
 git clone https://github.com/spayke3/lineage_build_unified lineage_build_unified -b patch-1
-git clone https://github.com/AndyCGYan/lineage_patches_unified lineage_patches_unified -b lineage-21-td
+rm -rf lineage_patches_unified
+git clone github.com/spayke3/lineage_patches_unified lineage_patches_unified -b patch-1
 
 bash lineage_build_unified/buildbot_unified.sh treble A64VN
